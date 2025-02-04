2c552f1e3910ae09b3f7102b248b401a55b55ec7
yl25946 <yl25946@students.mcpasd.k12.wi.us>
2025-02-03 21:35:08 -0600

    ok it works now
    bench: 5250755

--- src/search.cpp
+++ src/search.cpp
@@ -416,7 +416,8 @@ namespace stoat {
             const auto baseLmr = s_lmrTable[depth][std::min<u32>(legalMoves, 63)];
 
             if (!kRootNode && bestScore > -kScoreWin) {
-                if (!pos.isCapture(move) && !see::see(pos, move, -20 * depth * depth)) {
+                const auto seeThreshold = pos.isCapture(move) ? -100 * depth * depth : -20 * depth * depth;
+                if (!see::see(pos, move, seeThreshold)) {
                     continue;
                 }
 
